0045e5a3a877b377cea04a4374a8b481308061f2
Haohui Chen <76937112+YizhiHc@users.noreply.github.com>
2025-02-24 00:06:30 -0500

    test
    test

---
+++ iso_recipe/iso_recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iso_recipeApp.swift
+//  iso_recipe
+//
+//  Created by haohui chen on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct iso_recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iso_recipeTests/iso_recipeTests.swift
@@ -0,0 +1,16 @@
+//
+//  iso_recipeTests.swift
+//  iso_recipeTests
+//
+//  Created by haohui chen on 2/24/25.
+//
+
+import Testing
+
+struct iso_recipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
